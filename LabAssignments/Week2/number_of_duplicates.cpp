612e8d6d2777e42ce6961f617d4956757bb94788
Aadarsh <24971553+Aadarsh-Negi@users.noreply.github.com>
2022-04-12 22:58:14 +0530

    Update number_of_duplicates.cpp

--- LabAssignments/Week2/number_of_duplicates.cpp
+++ LabAssignments/Week2/number_of_duplicates.cpp
@@ -4,32 +4,6 @@
 #include<math.h>
 using namespace std;
 
-int jump(vector<int> &ar,int key){
-		
-		int n=ar.size();
-		int m=sqrt(n);
-		
-		int i=0;
-		while(i<n){
-				if(ar[i]==key) return 1;
-				
-				if(ar[i]>key){
-					for(int j=max(0,i-m)+1;j<i;j++)
-						if(ar[j]==key) return 1;
-				
-					return 0;
-				}
-				
-				if(i+m>=n){
-					for(;i<n;i++) if(ar[i]==key) return 1;
-					return 0;
-				}
-				i+=m;
-		}
-	return 0;
-}
-
-
 int main(){
 	
 	ios_base::sync_with_stdio(false);
