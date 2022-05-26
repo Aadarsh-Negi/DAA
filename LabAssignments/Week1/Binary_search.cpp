67107be5d6e1661cc30b4c7127f80b5ea208d682
ad <aadarshnegi.2@gmail.com>
2022-05-26 19:51:33 +0530

    DAA4_5

--- LabAssignments/Week1/Binary_search.cpp
+++ LabAssignments/Week1/Binary_search.cpp
@@ -1,49 +1,47 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 
 
 int main(){
 	
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	int n;
+	cin>>n;
+	vector<vector<int>> adj(n,vector<int>(n));
 	
-	int t;
-	cin>>t;
+	for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin>>adj[i][j];
 	
-	while(t--){
-		
-		int n;
-		cin>>n;
-		vector<int> ar(n);
-		
-		for(int &i:ar) cin>>i;
-		
-		int key;
-		cin>>key;
-		
-		
-		int cnt=0;
-		int l=0;
-		int r=n-1;
-		
-		while(l<=r){
-			int mid = l + (r-l)/2;
-			cnt++;
-			if(ar[mid]==key){
-					break;
-			}else if(ar[mid]>key) r=mid-1;
-			else l=mid+1;
-		}
+	int src,des;
+	cin>>src>>des;
+	src--;
+	des--;
 	
-		if(l<=r){
-			cout<<"Present "<<cnt<<"\n";
-		}else{
-			cout<<"Not Present "<<cnt<<"\n";
-		}
+	stack<int> st;
 	
-	
+	st.push(src);
+
+	while(st.size()){
+			int cur = st.top(); 
+			cout<<cur+1<<" "<<"\n";
+			for(int i=0;i<n;i++){
+				if(adj[cur][i]==1){
+					adj[cur][i]=2;
+					if(i==des){
+						cout<<"Yes Path Exists\n";
+						return 0;
+					}
+					st.push(i);	
+				}
+				
+			}
+		
 	}
+	cout<<"No Path does not exists";
+	
+	
+	
+	
 	
 	
 	
